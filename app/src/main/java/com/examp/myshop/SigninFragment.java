a201ed6045789efa803e807dce3028a4bba4653f
Sudhir <sudhir.singh@inferrix.com>
2020-04-05 22:07:24 +0530

    Initial commit

---
+++ app/src/main/java/com/examp/myshop/SigninFragment.java
@@ -0,0 +1,54 @@
+package com.examp.myshop;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SigninFragment extends Fragment {
+
+    public SigninFragment() {
+        // Required empty public constructor
+    }
+
+    private TextView dontHaveAccount;
+    private FrameLayout parentFrameLayout;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_signin, container, false);
+        dontHaveAccount = view.findViewById(R.id.tv_dont_hav_account);
+        parentFrameLayout = getActivity().findViewById(R.id.register_frameLayout);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dontHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new SignupFragment());
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/examp/myshop/SignupFragment.java
@@ -0,0 +1,56 @@
+package com.examp.myshop;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SignupFragment extends Fragment {
+
+    public SignupFragment() {
+        // Required empty public constructor
+    }
+
+    private TextView alreadyHavAnAccount;
+    FrameLayout parentFrameLayout;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        alreadyHavAnAccount = view.findViewById(R.id.tv_already_have_account);
+        parentFrameLayout = getActivity().findViewById(R.id.register_frameLayout);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        alreadyHavAnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new SigninFragment());
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+    }
+}
