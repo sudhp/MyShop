0da97aba074ed63856b1be70a6a96cd66d947b00
Sudhir <sudhir.singh@inferrix.com>
2020-04-11 20:07:03 +0530

    Part 10 completed. Forgot password functions are added.

--- app/src/main/java/com/examp/myshop/ResetPasswordFragment.java
+++ app/src/main/java/com/examp/myshop/ResetPasswordFragment.java
@@ -1,12 +1,28 @@
 package com.examp.myshop;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
@@ -18,11 +34,98 @@ public class ResetPasswordFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private EditText registerEmail;
+    private Button resetPasswordBtn;
+    private TextView goBack;
+    private FrameLayout parentFrameLayout;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reset_password, container, false);
+        View view = inflater.inflate(R.layout.fragment_reset_password, container, false);
+
+        registerEmail = view.findViewById(R.id.forgot_password_email);
+        resetPasswordBtn = view.findViewById(R.id.reset_password_button);
+        goBack = view.findViewById(R.id.tv_forgot_password_go_back);
+
+        parentFrameLayout = getActivity().findViewById(R.id.register_frameLayout);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        registerEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        resetPasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resetPasswordBtn.setEnabled(false);
+                resetPasswordBtn.setTextColor(Color.argb(50,255,255,255));
+
+                firebaseAuth.sendPasswordResetEmail(registerEmail.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(getActivity(),"Email Sent",Toast.LENGTH_SHORT).show();
+                                }else{
+                                    String error = task.getException().getMessage();
+                                    Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+                                }
+                                resetPasswordBtn.setEnabled(true);
+                                resetPasswordBtn.setTextColor(Color.rgb(255,255,255));
+                            }
+                        });
+            }
+        });
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new SigninFragment());
+            }
+        });
+
+    }
+
+    private void checkInputs(){
+        if(!TextUtils.isEmpty(registerEmail.getText())){
+            resetPasswordBtn.setEnabled(true);
+            resetPasswordBtn.setTextColor(Color.rgb(255,255,255));
+        }else{
+            resetPasswordBtn.setEnabled(true);
+            resetPasswordBtn.setTextColor(Color.rgb(255,255,255));
+        }
+
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_left,R.anim.slideout_from_left);
+        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
+        fragmentTransaction.commit();
     }
 }
